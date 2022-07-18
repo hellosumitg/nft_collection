f73816372abe2b535d021f95755d79543ee6b564
Sumit Kumar Gupta <hellosumitg@yahoo.com>
2022-07-18 13:52:44 +0530

    Update index.js

--- hardhat/constants/index.js
+++ hardhat/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x721DC2e6D44e9cEEf64941DC35bc753451002d11";
 // URL from where we extract the metadata for the Crypto Devs NFTs
-const METADATA_URL = "https://nft-collection-sumitdevtech.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-hellosumitg.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
