c1373a03e829d47a8be585532af620cf7ab058df
Sumit Kumar Gupta <hellosumitg@yahoo.com>
2022-07-18 13:57:52 +0530

    Update [tokenId].js

--- next-app/pages/api/[tokenId].js
+++ next-app/pages/api/[tokenId].js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
     // get the tokenId from the query params
     const tokenId = req.query.tokenId;
     // As all the images are uploaded on github, we can extract the images from github directly.
-    const image_url = "https://raw.githubusercontent.com/sumitdevtech/nft_collection/main/next-app/public/cryptodevs/";
+    const image_url = "https://raw.githubusercontent.com/hellosumitg/nft_collection/main/next-app/public/cryptodevs/";
     // The api is sending back metadata for a Crypto Dev
     // To make our collection compatible with Opensea, we need to follow some Metadata standards
     // when sending back the response from the api
