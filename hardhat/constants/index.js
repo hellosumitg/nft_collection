1ae7727614531d78f8f8f53e6347ba44d364e17d
Sumit Kumar Gupta <hellosumitg@yahoo.com>
2022-12-29 01:50:08 +0530

    Migrating to Goerli Testnet

--- hardhat/constants/index.js
+++ hardhat/constants/index.js
@@ -1,5 +1,5 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x721DC2e6D44e9cEEf64941DC35bc753451002d11";
+const WHITELIST_CONTRACT_ADDRESS = "0x5Aee10035E5d5901F78CA4955c26ce467b28EA4B";
 // URL from where we extract the metadata for the Crypto Devs NFTs
 const METADATA_URL = "https://nft-collection-hellosumitg.vercel.app/api/";
 
